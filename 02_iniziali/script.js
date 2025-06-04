c9365665a311df3725d94c2278c380915bff6133
matcir <matteocirilloboolean@gmail.com>
2025-06-04 14:33:47 +0200

    consegna snack 2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function iniziali (array) {
+    const initials = [];
+    for (let i=0; i < array.length; i++) {
+        initials[i] = array[i].charAt(0);
+    }
+    return initials;
+    
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const result = iniziali(names);
+console.log(result);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
